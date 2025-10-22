08ed77af3c9bdecb2c1f3f45d07acdb0fe93dfa0
Marcel <marcelin920@gmail.com>
2025-10-22 18:41:21 +0200

    initial commit

---
+++ src/main/java/com/dovskyy/wopfun/WopfunApplication.java
@@ -0,0 +1,13 @@
+package com.dovskyy.wopfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WopfunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WopfunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dovskyy/wopfun/WopfunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dovskyy.wopfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WopfunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
