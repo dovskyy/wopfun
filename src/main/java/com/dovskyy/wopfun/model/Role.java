aca8a576b59bba36d57e8b3898bedad45c8097b3
Marcel <marcelin920@gmail.com>
2025-10-29 02:18:10 +0100

    added USER and ADMIN role enum

---
+++ src/main/java/com/dovskyy/wopfun/model/Role.java
@@ -0,0 +1,6 @@
+package com.dovskyy.wopfun.model;
+
+public enum Role {
+    ADMIN,  // Administrator - pełny dostęp do systemu, może zarządzać użytkownikami
+    USER    // Zwykły użytkownik - dostęp do dzieci i dokumentów, bez zarządzania użytkownikami
+}
